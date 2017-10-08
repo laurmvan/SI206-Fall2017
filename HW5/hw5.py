de65d9e19853b474617e152e4f11e14aa950c24e
Lauren <laurmvan@umich.edu>
2017-10-08 13:34:27 -0400

    HW5

---
+++ HW5/hw5.py
@@ -0,0 +1,20 @@
+import re
+
+file1 = open('file2.txt')
+numbers = []
+for l in file1:
+	numbers.append(re.findall('[0-9]+',l))
+print (numbers)
+
+total = 0
+count = 0
+
+for list1 in numbers:
+	for string in list1:
+		number_str = int(string)
+		count += 1
+		total += number_str
+
+print (total)
+print (count)
+
