0f8cb75d9c24c0357a1f6505ff114eed9104dea4
Lauren <laurmvan@umich.edu>
2017-11-15 12:05:46 -0500

    proj3

--- 2015-08-12 13.16 Wednesday Webinar Series_  FINANCE Ready, Set, NETWORK!.py
+++
@@ -1,8 +0,0 @@
-counter = 1
-
-def doLotsOfStuff():
-	for i in (1, 2, 3): 
-		counter += 1
-
-doLotsOfStuff() 
-print(counter)
--- midterm.py
+++
@@ -1,9 +0,0 @@
-names1 = ['A', 'B']
-names2 = names1[:]
-print (names1[:])
-names3 = names2
-names2[0] = 'C'
-names3[1] = 'E'
-print (names1)
-print (names2)
-print (names3)
